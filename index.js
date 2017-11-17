ff6db8be9aec428fab95152f1ca9489f6c04759f
AAM77 <adeel.a.mhd@gmail.com>
2017-11-17 00:04:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
+
+function destructivelyUpdateObjectwithKeyAndValue(object, key, value) {
+  return object[key] = value;
+}
